da8d0623c77993c42606ed6942bb4d947b7781f5
martinlarrondo <mblarrondo@gmail.com>
2025-07-17 22:10:25 -0400

    lista
    se modifica la lista para mostrar los registros sincronizados desde el firebase

--- src/componentes/Lista.tsx
+++ src/componentes/Lista.tsx
@@ -11,14 +11,14 @@ const Lista = ({ registros, onEliminar, onEditar }: Props) => {
     <>
       Registro<br />
       {registros.map((reg) => (
-        <div key={reg.id}>
-          {reg.nombre}<br />
-          {reg.edad}<br />
-          {reg.genero}<br />
-          {reg.fecha}<br />
-          {reg.descripcion}<br />
+        <div key={reg.id} style={{ border: '1px solid #ccc', marginBottom: '10px', padding: '5px' }}>
+          <strong>Nombre:</strong> {reg.nombre}<br />
+          <strong>Edad:</strong> {reg.edad}<br />
+          <strong>Género:</strong> {reg.genero}<br />
+          <strong>Fecha de nacimiento:</strong> {reg.fecha}<br />
+          <strong>Descripción:</strong> {reg.descripcion}<br />
           <button onClick={() => onEditar(reg)}>Editar</button>
-          <button onClick={() => onEliminar(reg.id)}>Eliminar</button>
+          <button onClick={() => onEliminar(reg.id!)}>Eliminar</button>
         </div>
       ))}
     </>
